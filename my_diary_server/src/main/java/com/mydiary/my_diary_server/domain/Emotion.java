97771197b360a84dbde9bb8dd72fd840d38a4acf
dksalsrl <amgma1234@gmail.com>
2023-11-09 16:48:33 +0900

    diaryservice update

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/domain/Emotion.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/domain/Emotion.java
@@ -1,5 +1,17 @@
 package com.mydiary.my_diary_server.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import lombok.Getter;
+
 public enum Emotion {
-    ANDGRY, HAPPY
+    ANDGRY, 
+    HAPPY,
+    SAD;
+    
+	@JsonCreator
+	public static Emotion from(String value)
+	{
+		for()
+	}
 }
