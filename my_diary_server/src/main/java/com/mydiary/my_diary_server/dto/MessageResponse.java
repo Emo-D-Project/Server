4a6e67736563e3bb30739cc69f94d399c3e727b8
wintery7 <apddl5@naver.com>
2023-11-24 19:08:11 +0900

    0.0.20

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/dto/MessageResponse.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/dto/MessageResponse.java
@@ -1,18 +1,17 @@
 package com.mydiary.my_diary_server.dto;
 
 import com.mydiary.my_diary_server.domain.Message;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 public class MessageResponse {
     private final String content;
     private final Long senderId;
     private final Long receiverId;
+    private boolean isMyMessage;
     private final LocalDateTime sentAt;
 
     public MessageResponse(Message message) {
