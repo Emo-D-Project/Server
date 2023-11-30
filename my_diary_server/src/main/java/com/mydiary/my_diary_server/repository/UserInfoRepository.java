29e796faea3c351d73bb7f03613230cdfd67f7ba
wintery7 <apddl5@naver.com>
2023-11-30 18:41:16 +0900

    .

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/repository/UserInfoRepository.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/repository/UserInfoRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-   List<UserInfo> findAllByUserId(Long userId);
+    Optional<List<UserInfo>> findAllByUserId(Long userId);
+
 }
--- my_diary_server/src/main/java/com/mydiary/my_diary_server/service/UserInfoService.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/service/UserInfoService.java
@@ -25,7 +25,8 @@ public class UserInfoService {
 
 
     public List<UserInfoResponse> findAllById(Long id){
-        List<UserInfo> userInfos = userInfoRepository.findAllByUserId(id);
+        List<UserInfo> userInfos = userInfoRepository.findAllByUserId(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
         List<UserInfoResponse> userInfoResponses = new ArrayList<>();
         for (UserInfo userInfo : userInfos) {
@@ -34,24 +35,26 @@ public class UserInfoService {
         return userInfoResponses;
     }
 
-
-
     @Transactional
     public UserInfoResponse saveOrUpdate(UserInfo userInfo, Long userId) {
-        List<UserInfo> userInfos = userInfoRepository.findAllByUserId(userId);
+        /*List<UserInfo> userInfos = userInfoRepository.findAllByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + userId));
         boolean check = false;
         for (UserInfo userInfo1 : userInfos) {
             if(userInfo.equals(userInfo1)){
-               check = true;
-               userInfo1.update(userInfo);
-               return new UserInfoResponse(userInfo1);
-
+                check = true;
             }
         }
-        
-        if (userRepository.findById(userId).isPresent()) {
-            userInfo.setUserId(userRepository.findById(userId).get());
+
+        if(check){
+            UserInfo userInfo1 = userInfoRepository.findByUserId(userId).get();
+            userInfo1.update(userInfo);
+
+            return new UserInfoResponse(userInfo1);
         }
+        if (userRepository.findById(userId).isPresent()) {
+            userInfo.setUser(userRepository.findById(userId).get());
+        }*/
 
         return new UserInfoResponse(userInfoRepository.save(userInfo));
     }
