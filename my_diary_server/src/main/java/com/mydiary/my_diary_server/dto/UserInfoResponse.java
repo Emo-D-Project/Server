6ac360d54281c374685d134e8a658b523f67dd3b
wintery7 <apddl5@naver.com>
2023-12-02 12:35:27 +0900

    0.1.7

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/dto/UserInfoResponse.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/dto/UserInfoResponse.java
@@ -7,9 +7,11 @@ import lombok.Getter;
 public class UserInfoResponse {
     private String title;
     private String content;
+    private Long userId;
 
     public UserInfoResponse(UserInfo userinfo){
         this.title = userinfo.getTitle();
         this.content = userinfo.getContent();
+        this.userId = userinfo.getUser().getId();
     }
 }
