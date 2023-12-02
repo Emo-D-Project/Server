277a4ca9f85a6d544de1797ed4888c4a310ad19d
wintery7 <apddl5@naver.com>
2023-12-02 18:12:56 +0900

    0.1.8
    다이어리 오디오나 이미지 파일 없어도 저장할 수 있게 수정

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/controller/DiaryApiController.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/controller/DiaryApiController.java
@@ -1,5 +1,7 @@
 package com.mydiary.my_diary_server.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
 import com.mydiary.my_diary_server.domain.*;
 import com.mydiary.my_diary_server.dto.AddDiaryRequest;
 import com.mydiary.my_diary_server.dto.*;
@@ -10,6 +12,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,40 +24,101 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/diaries")
 public class DiaryApiController {
 
     private final DiaryService diaryService;
-       
-    @PostMapping(value = "create", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    @Operation(summary="일기 등록")
-    public ResponseEntity<Diary> addDiary
-            (@RequestPart AddDiaryRequest request, @RequestPart List<MultipartFile> imageFile,
-    		@RequestPart MultipartFile audioFile,
-    		Principal principal) throws IOException {
-    
-        Diary savedDiary = diaryService.save(request, imageFile, audioFile, principal.getName());
+    private final Storage storage;
+    @Value("${spring.cloud.gcp.storage.bucket}") // application.yml에 써둔 bucket 이름
+    private String bucketName;
 
-    	if(imageFile != null)
-    	{
-    		int i;
-    		System.out.println("이미지 개수" + imageFile.size());
-    		for(i=0; i<imageFile.size(); i++)
-    			System.out.println(imageFile.get(i).getOriginalFilename());
-    	}
-    	if(audioFile != null)
-    	{
-    		System.out.println(audioFile.getOriginalFilename());
-    	}
-    	
+    @PostMapping(value = "create")
+    @Operation(summary="일기 등록")
+    public ResponseEntity<Diary> addDiary(
+            @RequestBody AddDiaryRequest request,
+            @RequestPart(required = false) List<MultipartFile> imageFiles,
+            @RequestPart(required = false) MultipartFile audioFile,
+            Principal principal) throws IOException {
+
+        Diary diary = new Diary(Long.parseLong(principal.getName()), request.getContent(), request.getEmotion(), request.getIs_share(), request.getIs_comm());
+
+        String url = "https://storage.googleapis.com/emod_project/";
+
+        // 이미지 파일 처리
+        List<String> imageUrls = new ArrayList<>();
+
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            for (MultipartFile imageFile : imageFiles) {
+                // 이미지 파일 형식 확인 (예: image/jpeg)
+                String imageContentType = imageFile.getContentType();
+                if (imageContentType != null && imageContentType.startsWith("image/")) {
+                    // 이미지 파일을 처리하는 로직
+                    String imageUrl = handleImageFile(imageFile);
+                    imageUrls.add(imageUrl);
+                } else {
+                    // 이미지 파일 형식이 아닌 경우 처리 (예외 발생 또는 무시)
+                    System.out.println("Invalid image file format: " + imageContentType);
+                }
+            }
+        }
+        // 이미지 파일이 존재한다면 이미지 저장
+        if(!imageUrls.isEmpty()){
+            diary.setImages(imageUrls);
+        }
+        // 오디오 파일이 존재한다면 오디오 파일 저장
+        if (audioFile != null && !audioFile.isEmpty()) {
+            String audioUrl = handleAudioFile(audioFile);
+            diary.setAudio(audioUrl);
+        }
+
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedDiary);
+                .body(diary);
     }
-    
+
+    // 이미지 파일 처리 메소드
+    private String handleImageFile(MultipartFile imageFile) throws IOException {
+        String url = "https://storage.googleapis.com/emod_project/";
+        // 클라우드에 이미지 업로드
+        String uuid = UUID.randomUUID().toString();
+        String ext = imageFile.getContentType();
+
+        BlobInfo blobInfo = storage.create(
+                BlobInfo.newBuilder(bucketName, uuid)
+                        .setContentType(ext)
+                        .build(),
+                imageFile.getInputStream()
+        );
+
+        log.info("blobinfo/bucekt:  " + blobInfo.getBucket());
+
+        return url + uuid; // 실제 이미지 URL
+    }
+    // 오디오 파일 처리 로직
+    private String handleAudioFile(MultipartFile audioFile) throws IOException {
+        String url = "https://storage.googleapis.com/emod_project/";
+        // 클라우드에 오디오 업로드
+        String uuid = UUID.randomUUID().toString();
+        String ext = audioFile.getContentType();
+
+        BlobInfo blobInfo = storage.create(
+                BlobInfo.newBuilder(bucketName, uuid)
+                        .setContentType(ext)
+                        .build(),
+                audioFile.getInputStream()
+        );
+
+        log.info("blobinfo/bucekt: " + blobInfo.getBucket());
+
+        return url + uuid; // 실제 오디오 URL
+    }
+
     @GetMapping("/read")
     @Operation(summary="일기 전체 읽기")
     public ResponseEntity<List<DiaryResponse>> findAlldiaries() {
