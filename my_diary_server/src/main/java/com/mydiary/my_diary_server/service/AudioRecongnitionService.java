b6cfb83b602b7259a6950a421adfd757db5fb442
wintery7 <apddl5@naver.com>
2023-12-09 00:19:53 +0900

    0.1.18

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/service/AudioRecongnitionService.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/service/AudioRecongnitionService.java
@@ -1,81 +1,243 @@
 package com.mydiary.my_diary_server.service;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class AudioRecongnitionService {
 
-    public String run(String audioUrl){
-        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition";
-        String accessKey = "b2b17968-ad0f-4c70-a746-a7aa16b384ef";    // 발급받은 API Key
-        String languageCode = "korean";     // 언어 코드
-        String audioFilePath = audioUrl;  // 녹음된 음성 파일 경로
-        String audioContents = null;
+
+    public String PostTranscribeSample(MultipartFile audioFile) throws IOException, InterruptedException {
+        String accessToken = GetAccessToken();
+
+        // MultipartFile을 File로 변환
+        File convertedFile = convertMultipartFileToFile(audioFile);
+
+        // API 엔드포인트 및 HTTP 연결 설정
+        URL url = new URL("https://openapi.vito.ai/v1/transcribe");
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setRequestMethod("POST");
+        httpConn.setRequestProperty("accept", "application/json");
+        httpConn.setRequestProperty("Authorization", "Bearer " + accessToken);
+        httpConn.setRequestProperty("Content-Type", "multipart/form-data;boundary=authsample");
+        httpConn.setDoOutput(true);
+
+        try (DataOutputStream outputStream = new DataOutputStream(httpConn.getOutputStream())) {
+            // 파일 전송 부분 시작
+            outputStream.writeBytes("--authsample\r\n");
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"file\";filename=\"" + convertedFile.getName() + "\"\r\n");
+            outputStream.writeBytes("Content-Type: " + URLConnection.guessContentTypeFromName(convertedFile.getName()) + "\r\n");
+            outputStream.writeBytes("Content-Transfer-Encoding: binary" + "\r\n");
+            outputStream.writeBytes("\r\n");
+
+            // 파일 내용 전송
+            try (FileInputStream in = new FileInputStream(convertedFile)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            }
+
+            // 나머지 폼 데이터 전송
+            outputStream.writeBytes("\r\n");
+            outputStream.writeBytes("--authsample\r\n");
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"config\"\r\n");
+            outputStream.writeBytes("Content-Type: application/json\r\n");
+            outputStream.writeBytes("\r\n");
+            outputStream.writeBytes("{}");
+            outputStream.writeBytes("\r\n");
+            outputStream.writeBytes("--authsample\r\n");
+        }
+
+        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                ? httpConn.getInputStream()
+                : httpConn.getErrorStream();
+        Scanner s = new Scanner(responseStream).useDelimiter("\\A");
+        String response = s.hasNext() ? s.next() : "";
+        s.close();
+        System.out.println("파일 분석 바디" + response);
 
         Gson gson = new Gson();
+        IdResponse idResponse = gson.fromJson(response, IdResponse.class);
 
-        Map<String, Object> request = new HashMap<>();
-        Map<String, String> argument = new HashMap<>();
+        // 응답에서 id 값을 가져옴
+        String id = idResponse.getId();
 
-        try {
-            Path path = Paths.get(audioFilePath);
-            byte[] audioBytes = Files.readAllBytes(path);
-            audioContents = Base64.getEncoder().encodeToString(audioBytes);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("ID: " + id);
+        Thread.sleep((int) (convertedFile.length() / 1024) * 50L); // 5초 동안 코드를 지연
 
-        argument.put("language_code", languageCode);
-        argument.put("audio", audioContents);
+        return GetTranscribeSample(id,accessToken);
 
-        request.put("argument", argument);
-
-        URL url;
-        Integer responseCode = null;
-        String responBody = null;
-        try {
-            url = new URL(openApiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("POST");
-            con.setDoOutput(true);
-            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            con.setRequestProperty("Authorization", accessKey);
-
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.write(gson.toJson(request).getBytes("UTF-8"));
-            wr.flush();
-            wr.close();
-
-            responseCode = con.getResponseCode();
-            InputStream is = con.getInputStream();
-            byte[] buffer = new byte[is.available()];
-            int byteRead = is.read(buffer);
-            responBody = new String(buffer);
-
-            System.out.println("[responseCode] " + responseCode);
-            System.out.println("[responBody]");
-            System.out.println(responBody);
-
-            return "responBody: " + responBody;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "";
     }
 
+    public File convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
+        // MultipartFile을 File로 변환하는 부분
+        File file = File.createTempFile("temp", null);
+        multipartFile.transferTo(file);
+        return file;
+    }
+
+    public String GetAccessToken() throws IOException {
+        String CLIENT_ID = "XoWxCBzaDz0tsQ7WyXHH";
+        String CLIENT_SECRET = "i0VCNYdITLcERiAqZbzmjICf1yCqrvWyamFbzZYS";
+
+        URL url = new URL("https://openapi.vito.ai/v1/authenticate");
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setRequestMethod("POST");
+        httpConn.setRequestProperty("accept", "application/json");
+        httpConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        httpConn.setDoOutput(true);
+
+        String data = "client_id="+ CLIENT_ID +"&client_secret=" + CLIENT_SECRET;
+
+        byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+        OutputStream stream = httpConn.getOutputStream();
+        stream.write(out);
+
+        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                ? httpConn.getInputStream()
+                : httpConn.getErrorStream();
+        Scanner s = new Scanner(responseStream).useDelimiter("\\A");
+        String response = s.hasNext() ? s.next() : "";
+        s.close();
+        System.out.println(response);
+
+        Gson gson = new Gson();
+        AccessTokenResponse accessTokenResponse = gson.fromJson(response, AccessTokenResponse.class);
+
+        // 응답에서 access_token 값을 가져옴
+        String accessToken = accessTokenResponse.getAccessToken();
+
+        System.out.println("Access Token: " + accessToken);
+
+        return accessToken;
+    }
+
+    public String GetTranscribeSample(String id, String accessToken) throws IOException {
+        URL url = new URL("https://openapi.vito.ai/v1/transcribe/"+id);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setRequestMethod("GET");
+        httpConn.setRequestProperty("accept", "application/json");
+        httpConn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                ? httpConn.getInputStream()
+                : httpConn.getErrorStream();
+        Scanner s = new Scanner(responseStream).useDelimiter("\\A");
+        String response = s.hasNext() ? s.next() : "";
+        s.close();
+        System.out.println(response);
+
+        // JSON 파싱
+        Gson gson = new Gson();
+        ApiResponse apiResponse = gson.fromJson(response, ApiResponse.class);
+
+        // 파싱된 결과를 출력
+        System.out.println("id: " + apiResponse.getId());
+        System.out.println("status: " + apiResponse.getStatus());
+        System.out.println("verified: " + apiResponse.getResults().isVerified());
+
+        // utterances 배열의 첫 번째 요소에서 msg 값을 출력
+        String msg = apiResponse.getResults().getUtterances()[0].getMsg();
+        System.out.println("msg: " + msg);
+
+        return msg;
+    }
+}
+
+// ApiResponse 클래스 정의
+class ApiResponse {
+    private String id;
+    private String status;
+    private Results results;
+
+    // Getter 메서드 (생략)
+
+    public String getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Results getResults() {
+        return results;
+    }
+}
+
+// Results 클래스 정의
+class Results {
+    private Utterance[] utterances;
+    private boolean verified;
+
+    // Getter 메서드 (생략)
+
+    public Utterance[] getUtterances() {
+        return utterances;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+}
+
+// Utterance 클래스 정의
+class Utterance {
+    private int start_at;
+    private int duration;
+    private int spk;
+    private String spk_type;
+    private String msg;
+
+    // Getter 메서드 (생략)
+
+    public int getStart_at() {
+        return start_at;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getSpk() {
+        return spk;
+    }
+
+    public String getSpk_type() {
+        return spk_type;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
+class IdResponse {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+}
+
+
+class AccessTokenResponse {
+    private String access_token;
+    private long expire_at;
+
+    public String getAccessToken() {
+        return access_token;
+    }
+
+    public long getExpireAt() {
+        return expire_at;
+    }
 }
