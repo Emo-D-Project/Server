e5095dd7cc49a22dcc8f9101c476cc9cda71d8e3
dksalsrl <amgma1234@gmail.com>
2023-12-11 15:49:37 +0900

    aaee

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/stats/dto/ImageResponse.java
+++
@@ -1,5 +0,0 @@
-package com.mydiary.my_diary_server.dto;
-
-public class ImageResponse {
-	public String image;
-}
--- my_diary_server/src/main/java/com/mydiary/my_diary_server/stats5/dto/AddUserRequest.java
+++
@@ -1,9 +0,0 @@
-package com.mydiary.my_diary_server.dto;
-
-import lombok.*;
-
-@Data
-public class AddUserRequest {
-    private String password;
-    private String email;
-}
