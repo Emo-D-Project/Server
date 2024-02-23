e8b05567580d8ce48e954681c50e1b58ada727f0
wintery7 <apddl5@naver.com>
2024-02-23 12:07:23 +0900

    오류 해결 - 라이브러리 변화로 클래스의 사라짐 해결

--- my_diary_server/src/main/java/com/mydiary/my_diary_server/MyDiaryServerApplication.java
+++ my_diary_server/src/main/java/com/mydiary/my_diary_server/MyDiaryServerApplication.java
@@ -2,7 +2,6 @@ package com.mydiary.my_diary_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication()
 public class MyDiaryServerApplication {
 
