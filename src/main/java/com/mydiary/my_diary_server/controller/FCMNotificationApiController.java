5ffea6e70a98eaf7afa97ac92a7510933141780c
wintery7 <apddl5@naver.com>
2024-05-01 17:06:24 +0900

    Update FCMNotificationApiController.java

--- src/main/java/com/mydiary/my_diary_server/controller/FCMNotificationApiController.java
+++ src/main/java/com/mydiary/my_diary_server/controller/FCMNotificationApiController.java
@@ -23,11 +23,11 @@ public class FCMNotificationApiController {
     @Operation(summary = "알림 보내기")
     @PostMapping()
     public String sendNotificationByToken(@RequestBody FCMNotificationRequestDto requestDto){
-        return fcmNotificationService.sendNotificationByToken(requestDto);
+        return fcmNotificationService.sendNotificationByToken(requestDto); 
     }
 
 
- 
+
 
 
 }
