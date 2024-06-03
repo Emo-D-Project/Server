584b686d7a9a903c8fc9eea2eb733bb51eb6c0a0
dksalsrl <amgmal1234@gmail.com>
2024-06-03 16:29:26 +0900

    a111

--- src/main/java/com/mydiary/my_diary_server/test/RunRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.mydiary.my_diary_server.test;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record RunRequestDTO (
-        @JsonProperty("assistant_id")
-        String assistantId) {}
--- src/main/java/com/mydiary/my_diary_server/test/ThreadDTO.java
+++
@@ -1,5 +0,0 @@
-package com.mydiary.my_diary_server.test;
-
-public class ThreadDTO {
-
-}
